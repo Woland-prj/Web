b793483ad21e19d5c0d8808ff2b342e87a0704e1
Woland-prj <nzimin93@gmail.com>
2023-03-23 00:11:51 +0300

    :)

---
+++ Lab5/cmd/blog/handlers.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	ts, err := template.ParseFiles("pages/index.html")
+	if err != nil {
+		http.Error(w, "Internal Server Error", 500)
+		log.Println(err.Error())
+		return
+	}
+}
