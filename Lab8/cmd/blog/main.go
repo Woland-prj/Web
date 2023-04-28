1b6145bde7dadc7bfdab88cf3bd3ff17acececea
Woland-prj <nzimin93@gmail.com>
2023-04-29 00:12:40 +0300

    :)

---
+++ Lab8/cmd/blog/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql" // Импортируем для возможности подключения к MySQL
+	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
+)
+
+const (
+	port       = ":3000"
+	driverName = "mysql"
+)
+
+func main() {
+	db, err := openDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbx := sqlx.NewDb(db, driverName)
+
+	mux := mux.NewRouter()
+
+	mux.HandleFunc("/home", index(dbx))
+	mux.HandleFunc("/post/{postId}", post(dbx))
+
+	mux.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
+	log.Println("Start server at port " + port)
+	err = http.ListenAndServe(port, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func openDB() (*sql.DB, error) {
+	return sql.Open(driverName, "root:pdb7325fdah45@tcp(localhost:3306)/blog?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true")
+}
+
+/* func extractPass() string {
+	pass, exist := os.LookupEnv("MY_SQL_PASS")
+	if exist != true {
+		log.Fatal("password environment variable error")
+		return ""
+	}
+	return pass
+} */
