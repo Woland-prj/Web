a507a90c66581e0c278b8b304baec4cd3a49d9c9
Woland-prj <nzimin93@gmail.com>
2023-05-12 22:06:06 +0300

    :)

--- Lab8/static/scripts/admin-events.js
+++ Lab8/static/scripts/admin-events.js
@@ -1,5 +1,4 @@
-document.getElementById('files').addEventListener('change', handleFileSelect);
-function handleFileSelect(evt) {
+function loadAvatar(evt) {
     var file = evt.target.files;
     var f = file[0];
     if (!f.type.match('image.*')) {
@@ -9,7 +8,7 @@ function handleFileSelect(evt) {
     main.onload = (function(theFile) {
         return function(e) {
             var preview_main = document.getElementById('author-image-main');
-            preview_main.innerHTML = ['<img class="fields__upload-image_upload" "title="', escape(theFile.name), '" src="', e.target.result, '" />'].join('');
+            preview_main.innerHTML = ['<img class="fields__upload-image_author_load" "title="', escape(theFile.name), '" src="', e.target.result, '" />'].join('');
         };
     })(f);
     main.readAsDataURL(f);
@@ -17,9 +16,28 @@ function handleFileSelect(evt) {
     post.onload = (function(theFile) {
         return function(e) {
             var preview_post = document.getElementById('author-image-post');
-            preview_post.innerHTML = ['<img class="bottom__author-name_upload" "title="', escape(theFile.name), '" src="', e.target.result, '" />'].join('');
+            preview_post.innerHTML = ['<img class="bottom__author-name_load" "title="', escape(theFile.name), '" src="', e.target.result, '" />'].join('');
         };
     })(f);
     post.readAsDataURL(f);
 }
 
+function loadAvatar(evt) {
+    var file = evt.target.files;
+    var f = file[0];
+    if (!f.type.match('image.*')) {
+        alert("Image only please....");
+    }
+    var main = new FileReader();
+    main.onload = (function(theFile) {
+        return function(e) {
+            var preview_main = document.getElementById('image-big');
+            preview_main.classList.remove('fields__upload-image');
+            preview_main.innerHTML = ['<img class="fields__upload-image_post_load" "title="', escape(theFile.name), '" src="', e.target.result, '" />'].join('');
+        };
+    })(f);
+    main.readAsDataURL(f);
+}
+
+document.getElementById('author-pic-load').addEventListener('change', loadAvatar);
+document.getElementById('post-pic-load').addEventListener('change', loadAvatar);
