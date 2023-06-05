e00cd8c2f7a0ad4fae6cb4ad139de3bbb2334752
Woland-prj <nzimin93@gmail.com>
2023-06-05 21:09:02 +0300

    :)

--- Lab11/static/scripts/main-events.js
+++ Lab11/static/scripts/main-events.js
@@ -1,4 +1,6 @@
 const iconMenu = document.querySelector('.menu__icon');
+const menu = document.querySelector('.header__style');
+const scrollTrigger = 60;
 
 if(iconMenu) {
     const menuBody = document.querySelector('.menu__body');
@@ -6,5 +8,16 @@ if(iconMenu) {
         document.body.classList.toggle('_lock');
         iconMenu.classList.toggle('_active');
         menuBody.classList.toggle('_active');
+        // if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+        //     menu.classList.toggle('_scroll');
+        // }
     }) 
+}
+
+window.onscroll = function() {
+    if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+        menu.classList.add('_scroll');
+    } else {
+        menu.classList.remove('_scroll');
+    }
 }
